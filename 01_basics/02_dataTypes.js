be92f4df928779d770fda5a3e132a41bd72ee413
ShwetaSaini746 <108214151+ShwetaSaini746@users.noreply.github.com>
2024-03-16 17:36:28 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all JS code as newer version
+
+//alert("Hello"); // we are using node js, not browser
+
+console.log(3
+    +
+    3) // Code readability should be high.
+
+console.log("Hello");
+
+let name = "Shweta"
+let age = 19
+let isLoggedIn = false
+
+
+// premitive datatypes
+// number => 2 to the power 53
+// bigint (used in stock market etc)
+// string => ""
+// boolean => true/false
+// null => standalone value 
+// undefined => value is not yet assigned
+// symbol => for defining the uniqueness
+
+// object 
+
+console.log(typeof null); // returns "Object" (null is an object)
+console.log(typeof undefined); // returns undefined
